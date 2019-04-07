56e1cc4ebd485a74f1435f485949703aa64532f6
NK-AL\Nick <nkomarnicki@hotmail.com>
2019-04-07 15:56:11 -0400

    S4 L 147 - Adding tick to TankPlayerController
    Initial setup for raycasting every frame to check
    if our aiming reticle is hitting the landscape.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -14,7 +14,30 @@ void ATankPlayerController::BeginPlay() {
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const {	
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // Out parameter
+	if (GetSightRayHitLocation(HitLocation)) { //Has "side-effect", is going to line trace
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *(HitLocation.ToString()))
+		
+		// TODO Tell controlled tank to aim at this point
+	}
+}
+
+//Get world location of linetrace through crosshair, true if hits landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector & OutHitLocation) const
+{
+	OutHitLocation = FVector(1.0);
+	return true;
 }
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -16,8 +16,14 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-
+	virtual void Tick(float DeltaTime) override;
 	void BeginPlay() override;
 	ATank* GetControlledTank() const;
 	
+	//Start the tank moving the barrel so that a shot would hit where
+	//the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector &) const;
 };
